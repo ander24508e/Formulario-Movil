66bde495a98979cdff84ea0bebbac3b1b440e0a9
Ander <adeb2737@gmail.com>
2024-07-28 19:34:00 -0500

    Google_Maps coordenadas commit

--- lib/views/ubicacion_form.dart
+++ lib/views/ubicacion_form.dart
@@ -1,69 +1,35 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:formulario_movil/controllers/coordenadas.dart';
 
-class UbicacionForm extends StatefulWidget {
-  final Lugar? ubicacion; // Hacer que ubicacion sea opcional
-  const UbicacionForm({Key? key, this.ubicacion}) : super(key: key);
+class UbicacionForm extends StatelessWidget {
+  static String id = 'maps_page'; // Hacer que ubicacion sea opcional
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  Completer<GoogleMapController> _controller = Completer();
 
-  @override
-  State<UbicacionForm> createState() => _UbicacionFormState();
-}
-
-class _UbicacionFormState extends State<UbicacionForm> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
-  static const CameraPosition _initialCameraPosition = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+  static final CameraPosition _cameraPosition = CameraPosition(
+    target: LatLng(-0.24652791768463295, -79.17385722610584),
     zoom: 14.4746,
   );
 
-  static const CameraPosition _targetPosition = CameraPosition(
+  static final CameraPosition _targetPosition = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
-  Future<void> _goToTheTarget() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller
-        .animateCamera(CameraUpdate.newCameraPosition(_targetPosition));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.pushNamed(context, 'menu_form');
-          },
-        ),
-        title: Text(
-          'GOOGLE MAPS'.toUpperCase(),
-          style: GoogleFonts.dmSerifDisplay(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
+      key: _scaffoldKey,
+      body: Stack(
         children: [
-          Expanded(
-            child: GoogleMap(
-              mapType: MapType.normal,
-              initialCameraPosition: _initialCameraPosition,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-            ),
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _cameraPosition,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
           ),
         ],
       ),
